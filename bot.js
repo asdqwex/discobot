ba92739d023381ee1dc259bd1c1541f50c82658c
root <matthew.ellsworth@matthewellsworth.com>
2015-12-29 16:16:01 -0700

    azire imortalized

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ var DiscordClient = require('discord.io');
 var fs = require('fs');
 var array = fs.readFileSync('.secrets').toString().split("\n");
 
-console.log(array);
-
 var bot = new DiscordClient({
     email: array[0],
     password: array[1],
@@ -13,35 +11,40 @@ var bot = new DiscordClient({
 });
 
 bot.on('ready', function() {
-    console.log(bot.username + " - (" + bot.id + ")");
-});
-
-bot.setPresence({
-    game: "Serving Humanity since dec 28th, 2105"
-})
-
-bot.on('message', function(user, userID, channelID, message, rawEvent) {
-    if (message === "ping") {
-        bot.sendMessage({
-            to: channelID,
-            message: "pong"
+    console.log('connected');
+    console.log('joining channel');
+    bot.joinVoiceChannel("123201770355687424", function() {
+        bot.on('message', function(user, userID, channelID, message, rawEvent) {
+            if (message === "ping") {
+                bot.sendMessage({
+                    to: channelID,
+                    message: "pong"
+                });
+            } else if (message === "robot slave speak") {
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Here are the things I can do for you master: \n\nping - say pong\ngiphy <term> - post a damn giph\nwhere is azire - play random cody clip"
+                });
+            } else if (message.substring(0,5) === "giphy") {
+                gipher = message.slice(6);
+                theUrl = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+gipher;
+                var xmlHttp = new XMLHttpRequest();
+                xmlHttp.open( "GET", theUrl, false );
+                xmlHttp.send( null );
+                out = JSON.parse(xmlHttp.responseText);
+                bot.sendMessage({
+                    to: channelID,
+                    message: out.data.url
+                });
+            } else if (message.substring(0,14) == 'where is azire') {
+                clipList = fs.readdirSync('./Azire');
+                var item = clipList[Math.floor(Math.random()*clipList.length)];
+                item ='./Azire/'+item
+                bot.getAudioContext({ channel: "123201770355687424", stereo: true}, function(stream) {
+                console.log('play audio file', item);
+                        stream.playAudioFile(item); 
+                });
+            }
         });
-    } else if (message === "robot slave speak") {
-        bot.sendMessage({
-            to: channelID,
-            message: "Here are the things I can do for you master: \n\nping - say pong\ngiphy <term> - post a damn giph"
-        });
-    } else if (message.substring(0,5) === "giphy") {
-        gipher = message.slice(6);
-	console.log(gipher);
-	theUrl = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+gipher;
-	var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false );
-        xmlHttp.send( null );
-	out = JSON.parse(xmlHttp.responseText);
-	bot.sendMessage({
-            to: channelID,
-            message: out.data.url
-        });
-    }
+    });
 });
