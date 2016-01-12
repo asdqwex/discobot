02507fb862a5a2834e344513a310e67e70835a88
root <matthew.ellsworth@matthewellsworth.com>
2016-01-12 01:27:01 -0700

    fixes #8 ping module works

---
+++ src/modules/ping.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  names: ['ping'],
+  onMessage: function (bot, user, userID, channelID, message, rawEvent) {
+	 bot.sendMessage({
+          to: channelID,
+          message: 'pong, mother fucker'
+        })
+	
+  }
+}
