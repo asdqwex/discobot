4223aba25a228d09a4611d22766dd649a33a7065
Matthew Ellsworth <matthew.ellsworth@clickbank.com>
2016-01-14 10:48:01 -0700

    Fixes #16 aded xkcd feature

---
+++ src/modules/xkcd.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var request = require('request')
+
+module.exports = {
+  names: ['xkcd'],
+  onMessage: function (bot, user, userID, channelID, message, rawEvent) {
+    request('http://xkcd.com/info.0.json', function (msg, response, body) {
+      var img = JSON.parse(body).img
+      bot.sendMessage({
+        to: channelID,
+        message: img
+      })
+    })
+  }
+}
