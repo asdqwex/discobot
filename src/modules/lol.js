1e75da623c4dec2dace1e5ec162634c1c3a5645b
asdqwex <matthew.ellsworth@matthewellsworth.com>
2016-01-30 01:44:23 -0700

    initial config file implementation

--- src/modules/lol.js
+++ src/modules/lol.js
@@ -2,28 +2,25 @@
 
 var request = require('request')
 
-if (!process.env.RIOT_API_KEY) {
-  console.log('Missing Riot api key, this module will not be loaded.')
-} else {
-  module.exports = {
-    names: ['lol'],
-    onMessage: function (bot, user, userID, channelID, message, rawEvent) {
-      var verb = message.split(' ').splice(1).join()
-      if (verb === 'status') {
-        var reqUrl = 'http://status.leagueoflegends.com/shards/na'
-        request(reqUrl, function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-            var data = JSON.parse(body)
-            data.services.forEach(function (item) {
-              var statusMessage = 'NA ' + item.name + ' Status is ' + item.status
-              bot.sendMessage({
-                to: channelID,
-                message: statusMessage
-              })
+module.exports = {
+  names: ['lol'],
+  onMessage: function (bot, user, userID, channelID, message, rawEvent) {
+    var verb = message.split(' ').splice(1).join()
+    if (verb === 'status') {
+      var reqUrl = 'http://status.leagueoflegends.com/shards/na'
+      request(reqUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var data = JSON.parse(body)
+          data.services.forEach(function (item) {
+            var statusMessage = 'NA ' + item.name + ' Status is ' + item.status
+            bot.sendMessage({
+              to: channelID,
+              message: statusMessage
             })
-          }
-        })
-      }
+          })
+        }
+      })
     }
   }
 }
+
