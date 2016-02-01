5a22414c68616bb667ae6bc3a0a1f475a79aad24
chexerino <clint.thompson4@gmail.com>
2016-02-01 23:47:58 +0000

    cleaned up say.js

--- src/modules/say.js
+++ src/modules/say.js
@@ -4,7 +4,7 @@ module.exports = {
   names: ['say'],
   onMessage: function (bot, user, userID, channelID, message, rawEvent, {random}) {
     const ttsPhrase = (message.split('say').slice(1))
-      bot.sendMessage({
+    bot.sendMessage({
       to: channelID,
       message: `${ttsPhrase}`
     })
