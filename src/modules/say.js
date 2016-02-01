cdc02e60c86c899548fce6f0c52f5e09e99a7cb7
chexerino <clint.thompson4@gmail.com>
2016-02-01 23:50:20 +0000

    actually got #64 working

--- src/modules/say.js
+++ src/modules/say.js
@@ -5,6 +5,7 @@ module.exports = {
   onMessage: function (bot, user, userID, channelID, message, rawEvent, {random}) {
     const ttsPhrase = (message.split('say').slice(1))
     bot.sendMessage({
+      tts: true,
       to: channelID,
       message: `${ttsPhrase}`
     })
